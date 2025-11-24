a8ef2a9e4d49eadd0b819e82770fa19396401456
leehyerrr <leehyerrr@gmail.com>
2025-11-24 10:06:44 +0900

    infinite scroll

--- src/components/post/post-feed.tsx
+++ src/components/post/post-feed.tsx
@@ -1,21 +1,31 @@
-import { usePostData } from "@/hooks/queries/use-posts-data";
 import Fallback from "@/components/fallback";
 import Loader from "@/components/loader";
 import PostItem from "./post-items";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useInfinitePostsData } from "@/hooks/queries/use-infinite-posts-data";
 
 function PostFeed() {
-  const { data, error, isPending } = usePostData();
+  const { data, error, isPending, fetchNextPage, isFetchingNextPage } =
+    useInfinitePostsData();
+  const { ref, inView } = useInView();
 
-  console.log(data);
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
   if (error) return <Fallback />;
   if (isPending) return <Loader />;
 
   return (
     <div className="flex flex-col gap-10">
-      {data.map((post) => (
-        <PostItem key={post.id} {...post} />
-      ))}
+      {data.pages.map((page) =>
+        page.map((post) => <PostItem key={post.id} {...post} />),
+      )}
+      {isFetchingNextPage && <Loader />}
+      <div ref={ref}></div>
     </div>
   );
 }
