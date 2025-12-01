19bca6fa8131349240c3302471d59803f15623a7
leehyerrr <leehyerrr@gmail.com>
2025-12-01 20:55:45 +0900

    comment fetch + edit

--- src/components/comment/comment-list.tsx
+++ src/components/comment/comment-list.tsx
@@ -1,11 +1,23 @@
 import CommentItem from "@/components/comment/comment-item";
+import { useCommentData } from "@/hooks/queries/use-comments-data";
+import Fallback from "../fallback";
+import Loader from "../loader";
 
-export default function CommentList() {
+export default function CommentList({ postId }: { postId: number }) {
+  const {
+    data: comments,
+    error: fetchCommentsError,
+    isPending: isFetchCommentPending,
+  } = useCommentData(postId);
+
+  if (fetchCommentsError) return <Fallback />;
+  if (isFetchCommentPending) return <Loader />;
+
   return (
     <div className="flex flex-col gap-5">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      {comments?.map((comment) => (
+        <CommentItem key={comment.id} {...comment} />
+      ))}
     </div>
   );
 }
--- src/pages/post-detail-page.tsx
+++ src/pages/post-detail-page.tsx
@@ -13,8 +13,8 @@ export default function PostDetailPage() {
     <div className="flex flex-col gap-5">
       <PostItem postId={Number(postId)} type={"DETAIL"} />
       <div className="text-xl font-bold">댓글</div>
-      <CommentEditor postId={Number(postId)} />
-      <CommentList />
+      <CommentEditor type="CREATE" postId={Number(postId)} />
+      <CommentList postId={Number(postId)} />
     </div>
   );
 }
